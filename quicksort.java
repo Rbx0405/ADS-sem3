7239d8391103e7fcbff8a1f676e93fcad1d5db31
Rishit Butola <rishitbutola10@gmail.com>
2025-09-09 12:50:50 +0530

    sem 5 prog 1 quicksort

---
+++ quicksort.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class quicksort {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array (using Quick Sort):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
